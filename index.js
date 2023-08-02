5c1d54c23fe7d3cfde86593aee99914284300052
NaileaT <138537144+NaileaT@users.noreply.github.com>
2023-08-01 18:26:20 -0600

    Adición de la funcionalidad para desencriptar un texto

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ function encriptar () {
     let munheco = document.getElementById("munheco");
 
     let textoCifrado = texto
-                        .replace(/e/gi, "enter")
-                        .replace(/i/gi, "imes")
-                        .replace(/a/gi, "ai")
-                        .replace(/o/gi, "ober")
-                        .replace(/u/gi, "ufat");
+        .replace(/e/gi, "enter")
+        .replace(/i/gi, "imes")
+        .replace(/a/gi, "ai")
+        .replace(/o/gi, "ober")
+        .replace(/u/gi, "ufat");
 
     if (texto.length != 0) {
         document.getElementById("texto").value = textoCifrado;
@@ -24,3 +24,31 @@ function encriptar () {
     }
 }
 
+function desencriptar() {
+    let texto = document.getElementById("texto").value ;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let munheco = document.getElementById("munheco");
+
+
+    let textoCifrado = texto
+    .replace(/enter/gi, "e")
+    .replace(/imes/gi, "i")
+    .replace(/ai/gi, "a")
+    .replace(/ober/gi, "o")
+    .replace(/ufat/gi, "u");
+
+    if (texto.length != 0) {
+        document.getElementById("texto").value = textoCifrado;
+        tituloMensaje.textContent = "Texto Desencriptado con éxito";
+        parrafo.textContent = "";
+        munheco.src = "./img/desencriptado.jpg";
+    }else {
+        munheco.src = "./img/muhneco.jpg";
+        tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+        alert("Debes ingresar algún texto");
+    }
+
+
+}
